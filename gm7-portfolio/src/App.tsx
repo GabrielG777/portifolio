494e3777c6fcc5405c9814406f030d90d202de25
gaaaaaaabriel <gg405246@gmail.com>
2025-07-02 12:31:00 -0300

    projeto criado com vite, js e tw para estilização

---
+++ gm7-portfolio/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-orange-500">Portfólio Gm7 está no ar!</h1>
+    </div>
+  )
+}
